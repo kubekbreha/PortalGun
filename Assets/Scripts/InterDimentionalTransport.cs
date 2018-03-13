08d4cf6fe684785229dc4ddfbe73127639a9247f
kubekbreha <kubek.breha@gmail.com>
2018-03-13 15:02:16 +0100

    basic door example

---
+++ Assets/Scripts/InterDimentionalTransport.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class InterDimentionalTransport : MonoBehaviour {
+
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+		foreach (var mat in materials)
+			{
+				mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+			}
+	}
+
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.name != "Main Camera") 
+		{
+			return;		
+		}
+
+		// Outside of other world.
+		if(transform.position.z > other.transform.position.z)
+		{
+			Debug.Log("Outside of other world");
+			foreach (var mat in materials)
+			{
+				mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+			}
+		// Inside other dimension.
+		}
+		else 
+		{	
+			Debug.Log("Inside of other world");			
+			foreach (var mat in materials)
+			{
+				mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+			}
+		}
+	}
+
+	void OnDestroy()
+	{
+			foreach (var mat in materials)
+			{
+				mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+			}
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Shaders/PortalWindow.shader
@@ -0,0 +1,20 @@
+﻿Shader "Unlit/PortalWindow"
+{
+	SubShader{
+
+		ZWrite off
+		ColorMask 0
+
+		Stencil
+		{
+			Ref 1
+			Pass replace
+		}
+
+
+		Pass
+		{
+			
+		}
+	}
+}
