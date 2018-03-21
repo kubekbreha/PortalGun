7679c1526679aa5391fbe1ac4081672ad06f1167
kubekbreha <kubek.breha@gmail.com>
2018-03-21 16:49:08 +0100

    before AR kit , rotation,  base in unity

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,5 +12,12 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		float rotation = 0;
+		if(Input.GetKey(KeyCode.Q))
+			rotation -= 1;
+		if(Input.GetKey(KeyCode.E))
+			rotation += 1;
+		transform.Rotate(0, rotation, 0);
+			
 	}
 }
